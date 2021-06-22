293900ddc29af6c6a47b07195a4c5dcf0f76bcde
Mariumchonmon <Mariumakterch@gmail.com>
2021-06-22 22:36:49 +0600

    gridview

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
+
