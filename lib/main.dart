4e05671f703a48dd1a3a458146a1cd20136e696d
Mariumchonmon <Mariumakterch@gmail.com>
2021-07-01 23:52:03 +0600

    shopping cart and Displaying total and list cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import './screens/cart_screen.dart';
 import './screens/product_details_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
 import 'package:provider/provider.dart';
 import './providers/cart.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -29,6 +31,7 @@ class MyApp extends StatelessWidget {
      home: ProductOverviewScreen(),
      routes: {
      ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+       CartScreen.routeName: (ctx) => CartScreen(),
      },
     ),
     );
