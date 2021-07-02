7dd6aba190c0b4ebfb1eb757abec57bfbe6ad698
Mariumchonmon <Mariumakterch@gmail.com>
2021-07-02 23:27:36 +0600

    multiple input & disposing objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,6 +8,14 @@ static const routeName = '/edit-product';
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -31,6 +39,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Description'),
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              focusNode: _descriptionFocusNode,
             ),
           ],
         ),
