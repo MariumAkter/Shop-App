1eaf26ec8d983624efd29e8132a41dd87a9c6112
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-02 22:47:05 +0600

    adding user signup, allows user to login, handling authentication errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,27 +1,47 @@
 import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   late String _token;
   late DateTime _expiryDate;
   late String _userId;
-  Future<void> signup(String? email, String? password) async {
-final url =
-    Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWDAA4a0jGAYw36uRoO561f3NOXVfnvNU');
-final response = await http.post(
-  url,
-  body: json.encode(
-    {
-      'email': email,
-      'password': password,
-      'returnSecureToken': true,
 
-    },
-),
-);
-print(json.decode(response.body));
+  Future<void> _authenticate(String? email, String? password, String urlSegment) async {
+    final url =
+    Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDWDAA4a0jGAYw36uRoO561f3NOXVfnvNU');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null){
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch(error) {
+      throw error;
+    }
   }
-  
+
+  Future<void> signup(String? email, String? password) async {
+   return _authenticate( email, password, 'signUp');
+
+  }
+
+
+  Future<void> login(String? email, String? password) async{
+   return _authenticate( email, password, 'signInWithPassword');
+
+  }
+
+
 }
