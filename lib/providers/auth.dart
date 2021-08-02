bd3688d6ae5394defdbb1667519fb3aa0662c174
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-02 23:34:46 +0600

    managing the auth token locally

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,12 +1,28 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
-  late String _token;
-  late DateTime _expiryDate;
+   String? _token;
+   DateTime? _expiryDate;
   late String _userId;
+
+  bool get isAuth {
+    return token != null;
+
+  }
+
+  String? get token {
+    if (_expiryDate != null && _expiryDate!.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+
+  }
+
 
   Future<void> _authenticate(String? email, String? password, String urlSegment) async {
     final url =
@@ -27,6 +43,14 @@ class Auth with ChangeNotifier {
       if(responseData['error'] != null){
         throw HttpException(responseData['error']['message']);
       }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(Duration(
+        seconds: int.parse(responseData['expiresIn'],
+        ),
+      ),
+      );
+      notifyListeners();
     } catch(error) {
       throw error;
     }
